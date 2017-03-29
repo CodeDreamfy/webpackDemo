9c5118bf0ed4cf077251dbe4a4e7815a729b4f61
zhangyong <codedreamfy@outlook.com>
2017-03-29 11:43:53 +0800

    chunkPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 var webpack= require('webpack');
 module.exports = {
   entry: {
-    'example1.4': './example1.4',
-    'example1.5': './example1.5'
+    'main1': './main1',
+    'main2': './main2'
   },
   output: {
     // filename: '[name]-[chunkhash].js'
     filename: '[name]-[hash].js'
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common.js',['main1','main2'])
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'entry'
+    })
   ]
 }
